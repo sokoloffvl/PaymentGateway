6ee96e28381f26ccc75ffd3e50b4f19af61a7c7a
Vladimir Sokolov <vladimir.sokolov@geniussports.com>
2023-09-05 16:35:12 +0100

    Add domain unit tests project

---
+++ src/Test/Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Domain.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
