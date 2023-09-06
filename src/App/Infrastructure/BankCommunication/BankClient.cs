6b6275accfb0a70fac45c31ed6310ed95fa648c0
Vladimir Sokolov <vladimir.sokolov@geniussports.com>
2023-09-06 22:10:11 +0100

    Add mock bank client implementation

---
+++ src/App/Infrastructure/BankCommunication/BankClient.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Translators;
+using MockBank;
+using BankPaymentRequest = Domain.Models.BankPaymentRequest;
+using BankPaymentResponse = Domain.Models.BankPaymentResponse;
+
+namespace Infrastructure.BankCommunication;
+
+public class BankClient : IBankClient
+{
+    private readonly IMockBankClient mockBankClient;
+
+    public BankClient(IMockBankClient mockBankClient)
+    {
+        this.mockBankClient = mockBankClient;
+    }
+
+    public async Task<BankPaymentResponse> ProcessPayment(BankPaymentRequest paymentRequest)
+    {
+        var request = paymentRequest.ToMockRequest();
+        var response = await mockBankClient.ProcessPayment(request);
+        return response.ToDomain();
+    }
+}
---
+++ src/App/Infrastructure/Translators/BankRequestsTranslator.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Enums;
+using MockBank;
+using BankPaymentRequest = Domain.Models.BankPaymentRequest;
+using BankPaymentResponse = Domain.Models.BankPaymentResponse;
+using DeclineReason = Domain.Enums.DeclineReason;
+
+namespace Infrastructure.Translators;
+
+public static class BankRequestsTranslator
+{
+    public static MockBank.BankPaymentRequest ToMockRequest(this BankPaymentRequest request)
+    {
+        return new MockBank.BankPaymentRequest
+        {
+            PaymentId = request.PaymentId,
+            CardNumber = request.CardNumber,
+            CVV = request.CVV.ToString(),
+            CardOwner = request.CardOwner,
+            Amount = request.Amount,
+            Currency = request.Currency,
+            ValidToYear = request.ValidToYear,
+            ValidToMonth = request.ValidToMonth
+        };
+    }
+    
+    public static BankPaymentResponse ToDomain(this MockBank.BankPaymentResponse response)
+    {
+        return new BankPaymentResponse
+        {
+            PaymentId = response.PaymentId,
+            Status = response.Status.ToDomain(),
+            DeclineReason = response.DeclineReason.ToDomain()
+        };
+    }
+
+    private static PaymentStatus ToDomain(this BankPaymentStatus status)
+    {
+        return status switch
+        {
+            BankPaymentStatus.ProcessingFailed => PaymentStatus.ProcessingFailed,
+            BankPaymentStatus.Succeeded => PaymentStatus.Succeeded,
+            BankPaymentStatus.Declined => PaymentStatus.Declined,
+            _ => throw new ArgumentOutOfRangeException(nameof(status), status, "Unknown BankPaymentStatus")
+        };
+    }
+    
+    private static DeclineReason? ToDomain(this MockBank.DeclineReason? reason)
+    {
+        return reason switch
+        {
+            MockBank.DeclineReason.InsufficientFunds => DeclineReason.InsufficientFunds,
+            MockBank.DeclineReason.InvalidCardDetails => DeclineReason.InvalidCardDetails,
+            MockBank.DeclineReason.FraudDetected => DeclineReason.FraudDetected,
+            null => null,
+            _ => throw new ArgumentOutOfRangeException(nameof(reason), reason, "Unknown DeclineReason")
+        };
+    }
+}
