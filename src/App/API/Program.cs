fc45316b4d182ceb29b98fe13ee17e711a97e0d9
Vladimir Sokolov <vladimir.sokolov@geniussports.com>
2023-09-07 21:09:51 +0100

    Add docker support

--- src/App/API/Program.cs
+++ src/App/API/Program.cs
@@ -45,11 +45,8 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
